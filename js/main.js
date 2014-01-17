472cd1921f466dafa7f33bd296115807600feaa5
Kyle Stewart <kylestew@gmail.com>
2014-01-16 19:24:11 -0700

    Coming back to github pages from Parse hosting

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ $(function() {
 	$(".play-btn").click(function(){
 		event.preventDefault();
 		$("#modal").removeClass('modal-hide').addClass('modal-show');
-         
+         $("html, body").animate({ scrollTop: 0 }, "slow");
+  		return false;
 	});
 	$(".modal-bg").click(function(){
 		$("#modal").removeClass('modal-show').addClass('modal-hide');
